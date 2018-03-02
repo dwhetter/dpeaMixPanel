e8fa9b337571b1013ac68098ad72fa6409d197e6
Doug Whetter <doug.whetter@logmein.com>
2018-03-02 07:19:19 -0800

    initial commit

---
+++ dpea/utilities.py
@@ -0,0 +1,28 @@
+import requests
+import base64
+import json
+
+class MixPanel(object):
+    MIXPANEL_URL = "http://api.mixpanel.com/track/?data="
+    properties = {}
+
+    def __init__(self, projectName, token):
+        self.properties.clear()
+        self.addProperty("token", token)
+        self.addProperty("distinct_id", projectName)
+
+    def setEventName(self, eventName):
+        self.eventName = eventName
+
+    def addProperty(self, key, value):
+        self.properties[key] = value
+
+    def sendEvent(self):
+        event = {}
+        event['event'] = self.eventName;
+        event['properties'] = self.properties
+
+        data = json.dumps(event)
+        url = self.MIXPANEL_URL + base64.b64encode(data);
+        requests.post(url)
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dpea',
+      version='1.0',
+      description='MixPanel Wrapper for DPEA',
+      author='Doug',
+      author_email='doug@whetter.com',
+      packages=['dpea'],
+     )
